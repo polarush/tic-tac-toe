07d919137d3d829d04c076c4e262be97a6d70297
Yevgeniy Polyarush <polyarush.e@mail.ru>
2021-08-30 11:24:00 +0400

    feat: add comment, correct function

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -38,21 +38,44 @@ class TicTacToe {
 
 	getWinner() {
 		if ((this.matrix[0][0] === this.matrix[1][1]) && (this.matrix[0][0] === this.matrix[2][2]))  {
-			this.isWinner = this.matrix[0][0];
+			this.isWinner = this.matrix[0][0]; // первая линия
 			return this.isWinner;
 		} else if ((this.matrix[0][2] === this.matrix[2][0]) && (this.matrix[0][2] === this.matrix[1][1]))  {
-			this.isWinner = this.matrix[0][2];
+			this.isWinner = this.matrix[0][2]; // обратная диагональ
 			return this.isWinner;
 		}
-		for (let i = 0; i < this.matrix.length; i++) {
-			if (this.matrix.every((item) => (item[i] === this.matrix[i][i]))) {
-				this.isWinner = this.matrix[i][i];
-				return this.isWinner;
-			} else if (this.matrix[i].every((item) => (item === this.matrix[i][i]))) {
-				this.isWinner = this.matrix[i][i];
-				return this.isWinner;
-			} 
+
+		else if ((this.matrix[0][0] === this.matrix[1][0]) && (this.matrix[0][0] === this.matrix[2][0]))  {
+			this.isWinner = this.matrix[0][0]; // проверка на первый столбец
+			return this.isWinner;
 		}
+
+		else if ((this.matrix[0][1] === this.matrix[1][1]) && (this.matrix[0][1] === this.matrix[2][1]))  {
+			this.isWinner = this.matrix[0][1]; // проверка на второй столбец
+			return this.isWinner;
+		}
+
+		else if ((this.matrix[0][2] === this.matrix[1][2]) && (this.matrix[0][2] === this.matrix[2][2]))  {
+			this.isWinner = this.matrix[0][2]; // проверка на третий столбец
+			return this.isWinner;
+		}
+
+		else if ((this.matrix[0][0] === this.matrix[0][1]) && (this.matrix[0][0] === this.matrix[0][2]))  {
+			this.isWinner = this.matrix[0][1]; // проверка на первую линию
+			return this.isWinner;
+		}
+
+		else if ((this.matrix[1][0] === this.matrix[1][1]) && (this.matrix[1][0] === this.matrix[1][2]))  {
+			this.isWinner = this.matrix[1][1]; // проверка на вторую линию
+			return this.isWinner;
+		}
+
+		else if ((this.matrix[2][0] === this.matrix[2][1]) && (this.matrix[2][0] === this.matrix[2][2]))  {
+			this.isWinner = this.matrix[2][1]; // проверка на третью линию
+			return this.isWinner;
+		}
+
+
 		return this.isWinner;
 	}
 	noMoreTurns() {
